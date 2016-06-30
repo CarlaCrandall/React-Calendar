eb4c219177760b6e1be5278c9dbf5e2a51e2e93e
Carla Crandall <ccrandall@sapient.com>
2016-06-30 21:31:12 +0100

    Cleaning up structure

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Sidebar from './components/sidebar.jsx';
-import Content from './components/content.jsx';
+import Calendar from './components/calendar.jsx';
 
 var App = React.createClass({
 	render() {
 		return (
 			<div className="app">
 				<Sidebar />
-				<Content />
+				<Calendar />
 			</div>
 		)
 	}
