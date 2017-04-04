15e7249b7ea2be81e4ea3f529d7babb2abcfaa4b
Carla Crandall <carlalcrandall@gmail.com>
2017-04-04 19:17:00 -0400

    Fix linting issue

--- src/config/month-names.js
+++ src/config/month-names.js
@@ -1,5 +1,5 @@
 const MONTH_NAMES = [
-	"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
 ];
 
 export default MONTH_NAMES;
