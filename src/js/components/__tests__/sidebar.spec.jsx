ffb5b4988a7c52641e17e9fc5898b5eb07c666e7
Carla Crandall <carlalcrandall@gmail.com>
2017-04-14 11:43:43 -0400

    File cleanup and organization

--- src/js/components/__tests__/sidebar.spec.jsx
+++ src/js/components/__tests__/sidebar.spec.jsx
@@ -26,77 +26,83 @@ describe('Sidebar', () => {
         };
     });
 
-    it('should render without throwing an error', () => {
-        const component = shallowRender(globalProps);
-        expect(component.find('.sidebar').length).toEqual(1);
+
+    describe('Events', () => {
+        it('should call PREV_MONTH when the previous button is clicked', () => {
+            const
+                component = shallowRender(globalProps),
+                btn = component.find('.sidebar__button--previous');
+
+            btn.simulate('click');
+            expect(PREV_MONTH).toHaveBeenCalled();
+        });
+
+        it('should call NEXT_MONTH when the next button is clicked', () => {
+            const
+                component = shallowRender(globalProps),
+                btn = component.find('.sidebar__button--next');
+
+            btn.simulate('click');
+            expect(NEXT_MONTH).toHaveBeenCalled();
+        });
     });
 
-    it('should render the header when displayDatePicker is false', () => {
-        const component = shallowRender(globalProps);
-        expect(component.find('header').length).toEqual(1);
-    });
 
-    it('should not render the header when displayDatePicker is true', () => {
-        globalProps.displayDatePicker = true;
-        const component = shallowRender(globalProps);
-        expect(component.find('header').length).toEqual(0);
-    });
+    describe('Render Functions', () => {
+        it('should render without throwing an error', () => {
+            const component = shallowRender(globalProps);
+            expect(component.find('.sidebar').length).toEqual(1);
+        });
 
-    it('should render the content when date is defined', () => {
-        const component = shallowRender(globalProps);
-        expect(component.find('.sidebar__content').length).toEqual(1);
-    });
+        it('should render the header when displayDatePicker is false', () => {
+            const component = shallowRender(globalProps);
+            expect(component.find('header').length).toEqual(1);
+        });
 
-    it('should not render the content when date is null', () => {
-        globalProps.date = null;
-        const component = shallowRender(globalProps);
-        expect(component.find('.sidebar__content').length).toEqual(0);
-    });
+        it('should not render the header when displayDatePicker is true', () => {
+            globalProps.displayDatePicker = true;
+            const component = shallowRender(globalProps);
+            expect(component.find('header').length).toEqual(0);
+        });
 
-    it('should render the EventList when data has loaded and there is no error', () => {
-        const component = shallowRender(globalProps);
-        expect(component.find(EventList).length).toEqual(1);
-    });
+        it('should render the content when date is defined', () => {
+            const component = shallowRender(globalProps);
+            expect(component.find('.sidebar__content').length).toEqual(1);
+        });
 
-    it('should not render the EventList when eventsByDate is null', () => {
-        globalProps.eventsByDate = null;
-        const component = shallowRender(globalProps);
-        expect(component.find(EventList).length).toEqual(0);
-    });
+        it('should not render the content when date is null', () => {
+            globalProps.date = null;
+            const component = shallowRender(globalProps);
+            expect(component.find('.sidebar__content').length).toEqual(0);
+        });
 
-    it('should not render the EventList when loading is true', () => {
-        globalProps.loading = true;
-        const component = shallowRender(globalProps);
-        expect(component.find(EventList).length).toEqual(0);
-    });
+        it('should render the EventList when data has loaded and there is no error', () => {
+            const component = shallowRender(globalProps);
+            expect(component.find(EventList).length).toEqual(1);
+        });
 
-    it('should not render the EventList when error is true', () => {
-        globalProps.error = true;
-        const component = shallowRender(globalProps);
-        expect(component.find(EventList).length).toEqual(0);
-    });
+        it('should not render the EventList when eventsByDate is null', () => {
+            globalProps.eventsByDate = null;
+            const component = shallowRender(globalProps);
+            expect(component.find(EventList).length).toEqual(0);
+        });
 
-    it('should render the error message when error is true', () => {
-        globalProps.error = true;
-        const component = shallowRender(globalProps);
-        expect(component.find('.sidebar__message').length).toEqual(1);
-    });
+        it('should not render the EventList when loading is true', () => {
+            globalProps.loading = true;
+            const component = shallowRender(globalProps);
+            expect(component.find(EventList).length).toEqual(0);
+        });
 
-    it('should call PREV_MONTH when the previous button is clicked', () => {
-        const
-            component = shallowRender(globalProps),
-            btn = component.find('.sidebar__button--previous');
+        it('should not render the EventList when error is true', () => {
+            globalProps.error = true;
+            const component = shallowRender(globalProps);
+            expect(component.find(EventList).length).toEqual(0);
+        });
 
-        btn.simulate('click');
-        expect(PREV_MONTH).toHaveBeenCalled();
-    });
-
-    it('should call NEXT_MONTH when the next button is clicked', () => {
-        const
-            component = shallowRender(globalProps),
-            btn = component.find('.sidebar__button--next');
-
-        btn.simulate('click');
-        expect(NEXT_MONTH).toHaveBeenCalled();
+        it('should render the error message when error is true', () => {
+            globalProps.error = true;
+            const component = shallowRender(globalProps);
+            expect(component.find('.sidebar__message').length).toEqual(1);
+        });
     });
 });
--- src/js/components/__tests__/spinner.spec.jsx
+++ src/js/components/__tests__/spinner.spec.jsx
@@ -5,35 +5,44 @@ import { Spinner } from '../';
 describe('Spinner', () => {
     const shallowRender = () => shallow(<Spinner />);
 
-    it('should render without throwing an error', () => {
-        const component = shallowRender();
-        expect(component.find('.spinner').length).not.toEqual(0);
+
+    describe('Component Lifecycle', () => {
+        it('should call focus when componentDidMount is called', () => {
+            const
+                component = shallowRender(),
+                instance = component.instance(),
+                focus = jest.fn();
+
+            instance.spinner = { focus };
+            instance.componentDidMount();
+
+            expect(focus).toHaveBeenCalled();
+        });
+
+        it('should set the spinner ref to null', () => {
+            const component = shallowRender();
+            expect(component.instance().spinner).toEqual(null);
+        });
     });
 
-    it('should call focus when componentDidMount is called', () => {
-        const
-            component = shallowRender(),
-            instance = component.instance(),
-            focus = jest.fn();
 
-        instance.spinner = { focus };
-        instance.componentDidMount();
+    describe('Custom Functions', () => {
+        it('should set the spinner ref to passed in element when refHandler is called', () => {
+            const
+                component = shallowRender(),
+                instance = component.instance(),
+                fakeElement = 'TEST';
 
-        expect(focus).toHaveBeenCalled();
+            instance.refHandler(fakeElement);
+            expect(instance.spinner).toEqual(fakeElement);
+        });
     });
 
-    it('should set spinner to null', () => {
-        const component = shallowRender();
-        expect(component.instance().spinner).toEqual(null);
-    });
 
-    it('should set spinner to passed in element when refHandler is called', () => {
-        const
-            component = shallowRender(),
-            instance = component.instance(),
-            fakeElement = 'TEST';
-
-        instance.refHandler(fakeElement);
-        expect(instance.spinner).toEqual(fakeElement);
+    describe('Render Functions', () => {
+        it('should render without throwing an error', () => {
+            const component = shallowRender();
+            expect(component.find('.spinner').length).toEqual(1);
+        });
     });
 });
